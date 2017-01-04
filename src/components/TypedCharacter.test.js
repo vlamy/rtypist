3f486bc12dee4c784e36165ac37b708afe208f53
Willy Malvault <wmalvault@leadformance.com>
2017-01-04 22:23:54 +0100

    test(typing): add TypingCharacter and tracker

--- src/components/TypedCharacter.test.js
+++ src/components/TypedCharacter.test.js
@@ -1,12 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TypedCharacter from './TypedCharacter';
+import ReactTestUtils from 'react-addons-test-utils';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<TypedCharacter
-    key="mockKey"
-    character="c"
-    status="to-go"
+    key='mockKey'
+    character='c'
+    status='to-go'
   />, div);
+});
+
+it('renders a classic character with status to-go', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character='c'
+        status='to-go'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual('c');
+    expect(result.props.className).toEqual('typed-character char-to-go');
+});
+
+it('renders a classic character with status current', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character='c'
+        status='current'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual('c');
+    expect(result.props.className).toEqual('typed-character char-current');
+});
+
+it('renders a classic character with status error', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character='c'
+        status='error'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual('c');
+    expect(result.props.className).toEqual('typed-character char-error');
+});
+
+it('renders a classic character with status success', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character='c'
+        status='success'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual('c');
+    expect(result.props.className).toEqual('typed-character char-success');
+});
+
+it('renders a space character with special classname', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character=' '
+        status='success'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual(' ');
+    expect(result.props.className).toEqual('typed-character char-success char-space');
+});
+
+it('renders a line break with special classname', () => {
+    const renderer = ReactTestUtils.createRenderer();
+    const lineBreakChar = '\n';
+    renderer.render(<TypedCharacter
+        key='mockKey'
+        character={lineBreakChar}
+        status='success'
+    />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.children).toEqual('\n');
+    expect(result.props.className).toEqual('typed-character char-success char-cr');
 });
--- src/components/TypingTracker.test.js
+++ src/components/TypingTracker.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TypingTracker from './TypingTracker';
+import ReactTestUtils from 'react-addons-test-utils';
+
+const divClassName = 'rtypist__typing-tracker';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<TypingTracker />, div);
+});
+
+
+it(`renders a div with class ${divClassName} `, () => {
+    const renderer = ReactTestUtils.createRenderer();
+    renderer.render(<TypingTracker />);
+    const result = renderer.getRenderOutput();
+    expect(result.type).toBe('div');
+    expect(result.props.className).toEqual(divClassName);
 });
