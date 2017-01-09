98f51142f07a330c28593ef7d9b11ed9f514c3bd
Willy Malvault <wmalvault@leadformance.com>
2017-01-09 20:16:07 +0100

    fix(TypingTracker) adding stats mock for test

--- src/components/typing/TypingTracker.test.js
+++ src/components/typing/TypingTracker.test.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TypingTracker from './TypingTracker';
 import ReactTestUtils from 'react-addons-test-utils';
+import Exercise from '../exercises/Exercise';
 
 const divClassName = 'rtypist__typing-tracker';
+const exercise = new Exercise('stats-test', 'asdflhqueoeuthfaljasuhfue');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<TypingTracker />, div);
+  ReactDOM.render(<TypingTracker exercise={exercise}/>, div);
 });
 
 
 it(`renders a div with class ${divClassName} `, () => {
     const renderer = ReactTestUtils.createRenderer();
-    renderer.render(<TypingTracker />);
+    renderer.render(<TypingTracker exercise={exercise}/>);
     const result = renderer.getRenderOutput();
     expect(result.type).toBe('div');
     expect(result.props.className).toEqual(divClassName);
