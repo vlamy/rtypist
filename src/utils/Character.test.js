a548394c67d44152f10f2e7ee0c7760600496b2e
Willy Malvault <wmalvault@leadformance.com>
2017-01-31 15:42:09 +0100

    add character model

---
+++ src/utils/Character.test.js
@@ -0,0 +1,90 @@
+import TYPING_STATUS from '../constants/TYPING_STATUS';
+import Character from './Character';
+
+describe('utils/Character', () => {
+  let character;
+
+  beforeEach(() => {
+    character = new Character('a');
+  });
+
+  describe('#constructor', () => {
+    it('builds without crashing', () => {
+      expect(character).not.toBeNull();
+    });
+  });
+
+  describe('#getStatus', () => {
+    describe('when the character has not been typed yet', () => {
+      it('return status togo', () => {
+        expect(character.getStatus()).toEqual(TYPING_STATUS.togo);
+      });
+    });
+
+    describe('when the correct character has been typed', () => {
+      beforeEach(() => {
+        character.setLastTypedValue('a');
+      });
+
+      it('return status success', () => {
+        expect(character.getStatus()).toEqual(TYPING_STATUS.success);
+      });
+    });
+
+    describe('when a wrong character has been typed', () => {
+      beforeEach(() => {
+        character.setLastTypedValue('b');
+      });
+
+      it('return status success', () => {
+        expect(character.getStatus()).toEqual(TYPING_STATUS.error);
+      });
+    });
+  });
+
+  describe('#getCharToDisplay', () => {
+    describe('when the character has not been typed yet', () => {
+      it('return the initial character', () => {
+        expect(character.getCharToDisplay()).toEqual('a');
+      });
+    });
+
+    describe('when the correct character has been typed', () => {
+      beforeEach(() => {
+        character.setLastTypedValue('a');
+      });
+
+      it('return the initial character', () => {
+        expect(character.getCharToDisplay()).toEqual('a');
+      });
+    });
+
+    describe('when a wrong character has been typed', () => {
+      beforeEach(() => {
+        character.setLastTypedValue('b');
+      });
+
+      it('return the typed character', () => {
+        expect(character.getCharToDisplay()).toEqual('b');
+      });
+    });
+  });
+
+  describe('#setLastTypedValue', () => {
+    describe('when the character has not been typed yet', () => {
+      it('return the initial character', () => {
+        expect(character.lastTypedValue).toBeNull();
+      });
+    });
+
+    describe('when a character has been typed', () => {
+      beforeEach(() => {
+        character.setLastTypedValue('b');
+      });
+
+      it('return the initial character', () => {
+        expect(character.lastTypedValue).toEqual('b');
+      });
+    });
+  });
+});
